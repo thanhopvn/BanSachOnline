a201731b67378ffffe859259310d0da7a5ec0ef2
Truong Thanh <61083809+thanhopvn@users.noreply.github.com>
2021-06-26 23:17:18 +0700

    Ban Sach Online

---
+++ Admin/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Copyright &copy;</b> 2020
+        </div>
+        <strong>Đồ án lập trình web nâng cao - Nhóm 20</strong>
---
+++ footer.php
@@ -0,0 +1,30 @@
+
+	<footer>
+		<div class="container">
+			<div class="wrap-footer">
+				<div class="row">
+					<div class="col-md-3 col-footer footer-1">
+						<h5>Nhà sách online</h5>
+						<p>Nhà sách online với rất nhiều đầu sách hấp dẫn cho bạn đọc lựa chọn.</p>
+					</div>
+					</div>
+					<p><span class="glyphicon glyphicon-home"></span>Hà Nội, Việt Nam.</p>
+					
+			</div>
+		</div>
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						 Copyright © 2021
+
+					</div>
+					<div class="col-md-6">
+						<div class="pull-right">
+							Đồ án lập trình web nâng cao - Nhóm 20
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
